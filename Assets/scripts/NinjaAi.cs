e2883a5740a9ff7401abe6f632d0cd4b81779a98
Shadowpath98 <johntdnd@gmail.com>
2019-08-04 20:33:07 -0400

    Ninja coding

---
+++ Assets/scripts/NinjaAi.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:bdc16bcf2ed51c14d9fba47ebc2c3800ab647e94e38973734f462a1736a86081
+size 5091
---
+++ Assets/scripts/NinjaKnife.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:23565c477f30d48446ab0ade741f150dea0ff98f2d6a0e85f4c3ff3f992d0935
+size 1874
